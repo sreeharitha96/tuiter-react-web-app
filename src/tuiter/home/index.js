c1a8708a354a1314258cce81e388a6771eca943d
sreeharitha1396 <vempati.sr@northeastern.edu>
2022-10-23 18:09:52 -0400

    challenge added

--- src/tuiter/home/index.js
+++ src/tuiter/home/index.js
@@ -1,46 +1,48 @@
 
-
-
-
 const HomeComponent = () => {
     return (<>
         <div class="container">
             <div class="row">
-                <div class="col-3">
-                    <img src="/images/rocket.png" className="w-100" />
+                <div class="col-3 border-bottom">
+                    <img src="/images/rocket.png" className="w-100 rounded-circle" />
                 </div>
-                <div class="col-9">
+                <br/><br/> 
+                <div class="col-9 border-bottom">
                     <p>Elon Mask Retweeted</p>
-                    <p><b>SpaceX</b><i class="fa fa-check-circle" aria-hidden="true"></i>
-                    @SpaceX . 23h
+                    <p><b>SpaceX</b> <i class="bi bi-check-circle" aria-hidden="true"></i> @SpaceX . 23h
                     </p>
                     <p>Dennis and Akkio Tito are the first two crew members on Startship's second
-                        commercial spaceflightaround the Moon <i class="fa fa-arrow-right" aria-hidden="true"></i> <a href="url">spacex.com/updates</a> 
+                        commercial spaceflightaround the Moon <i class="bi bi-arrow-right" aria-hidden="true"></i> <a href="url">spacex.com/updates</a> 
                     </p>
-                    <img src="/images/rocket.png" className="w-100"/>
+                    <img src="/images/rocket.png" className="w-100 rounded"/>
+                    <br/>
+                    <br/>
                     <p>
-                    <i class="fa fa-comments-o" aria-hidden="true"></i> 595 <i class="fa fa-retweet" aria-hidden="true"></i> 1,368
-                    <i class="fa fa-heart" aria-hidden="true"></i> 11.7K <i class="fa fa-share-alt" aria-hidden="true"></i>
+                    <span class="bi bi-chat">595</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <span class="bi bi-repeat" aria-hidden="true"> 1,368</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <span class="bi bi-heart" aria-hidden="true">11.7K </span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <span class="bi bi-box-arrow-up"></span>
                     </p>
                     <a href="url">Show this thread</a>
                     <br/><br/> 
                 </div>
-                
+                <br/><br/> <br/><br/> 
                 <div class="col-3">
-                    <img src="/images/rocket.png" className="w-100" />
+                    <img src="/images/rocket.png" className="w-100 rounded-circle" />
                 </div>
                 <div class="col-9">
                     <p>Elon Mask Retweeted</p>
-                    <p><b>SpaceX</b><i class="fa fa-check-circle" aria-hidden="true"></i>
-                    @SpaceX . 23h
+                    <p><b>SpaceX</b>  <i class="bi bi-check-circle" ></i> @SpaceX . 23h
                     </p>
                     <p>Dennis and Akkio Tito are the first two crew members on Startship's second
-                        commercial spaceflightaround the Moon <i class="fa fa-arrow-right" aria-hidden="true"></i> <a href="url">spacex.com/updates</a> 
+                        commercial spaceflightaround the Moon <i class="bi bi-arrow-right"></i> <a href="url">spacex.com/updates</a> 
                     </p>
                     
-                    <p>
-                    <i class="fa fa-comments-o" aria-hidden="true"></i> 595 <i class="fa fa-retweet" aria-hidden="true"></i> 1,368
-                    <i class="fa fa-heart" aria-hidden="true"></i> 11.7K <i class="fa fa-share-alt" aria-hidden="true"></i>
+                    <p class="icons">
+                    <span class="bi bi-chat">595</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <span class="bi bi-repeat" > 1,368</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <span class="bi bi-heart">11.7K </span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <span class="bi bi-box-arrow-up"></span>
                     </p>
                 
                 </div>
