fe98ec4f9aea157f58afaae0b74cfc571ff5bd83
sreeharitha1396 <vempati.sr@northeastern.edu>
2022-11-29 22:53:23 -0500

    mongodb error resolved

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const TUITS_API = 'https://tuiter-node-server-app.herokuapp.com/api/tuits';
+const TUITS_API = 'https://tuiter-node-server-sh-app.herokuapp.com/api/tuits';
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
